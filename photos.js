56c2eca70e67562c67fcc73d3756613d29fdf65c
marcin.borek <marcin.borek82@gmail.com>
2012-09-19 18:08:23 +0200

    dd favourite icon

--- photos.js
+++ photos.js
@@ -23,8 +23,10 @@ var setupPhotos = (function ($) {
             var i;
             for (i = 0; i < max; i += 1) {
                 photos.push(data.items[i].media.m);
+               
             }
-            callback(null, photos);
+         
+            callback(null, photos); 
         };
 
         $.ajax({
@@ -43,13 +45,11 @@ var setupPhotos = (function ($) {
         var results = [];
         function handleResult (err, photos) {
             if (err) { return callback(err); }
-
             results.push(photos);
             if (results.length === tags.length) {
                 callback(null, flatten(results));
             }
         }
-
         each(tags, function (tag) {
             loadPhotosByTag(tag, max, handleResult);
         });
@@ -58,6 +58,7 @@ var setupPhotos = (function ($) {
     function renderPhoto (photo) {
         var img = new Image();
         img.src = photo;
+        img.id  = photo.substr(photo.lastIndexOf('/')+1,21);
         return img;
     }
 
@@ -67,6 +68,41 @@ var setupPhotos = (function ($) {
             var elm = document.createElement('div');
             elm.className = 'photo';
             elm.appendChild(img);
+            /**   Favorite icon  ******/
+            var elm2 = document.createElement('img');
+            elm2.className = 'starIcon';
+            elm2.id = 'star_' + img.id;
+            //read cookie
+            var cookieArray = document.cookie.split("; ");
+            
+            var cookieItem = '';
+            elm2.src = 'favorites.png';	
+            jQuery.each(cookieArray, function() {
+            		cookieItem = this.split("=");
+            		
+                    if(cookieItem[0] == 'favoriteImage' && cookieItem[1] == elm2.id) {
+                    	elm2.src = 'favorites_active.png';
+                     	return;
+                     }
+            });
+
+            
+           
+          
+            //action after click on favorite icon
+            $(elm2).bind('click', function() {
+            	
+            	//set favorite icon
+            	$('.starIcon').each(function(index) {
+            			this.src = 'favorites.png';
+            	});
+            	this.src = 'favorites_active.png';
+            	//set favorite image
+            	document.cookie = "favoriteImage=" + elm2.id;
+            });
+            
+            elm.appendChild(elm2);
+            
             holder.appendChild(elm);
         };
     }
@@ -77,9 +113,10 @@ var setupPhotos = (function ($) {
     return function setup (tags, callback) {
         loadAllPhotos(tags, max_per_tag, function (err, items) {
             if (err) { return callback(err); }
-
             each(items.map(renderPhoto), imageAppender('photos'));
             callback();
         });
     };
 }(jQuery));
+
+
